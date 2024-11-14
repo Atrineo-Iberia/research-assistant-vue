0166426e47610c16526ea9746b798bc654cb3f97
alber0087 <albertorg0087@gmail.com>
2024-11-14 09:36:12 +0000

    upload files service updated

--- src/api/uploadFiles.js
+++ src/api/uploadFiles.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const API_BASE_URL = 'https://research-assistant-backenduvicorn-main.onrender.com'
+
 export const uploadFiles = async (files) => {
   const formData = new FormData()
   files.forEach(file => {
@@ -7,10 +9,11 @@ export const uploadFiles = async (files) => {
   })
 
   try {
-    const response = await axios.post('/api/upload-files', formData, {
+    const response = await axios.post(`${API_BASE_URL}/api/upload-files`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
-      }
+      },
+      withCredentials: true
     })
     return response.data
   } catch (error) {
